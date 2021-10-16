5105160d217c956c4fe3d7c51634963b1ac8f907
MattC <MattC@192.168.1.3>
2021-10-16 18:45:41 -0400

    Added comments to part B

--- partB.m
+++ partB.m
@@ -1,5 +1,8 @@
 clear all; close all;
 
+% Find the threshold
 thres = intermeans_12('rice.png');
+
+% Display the results
 disp(['Normalized threshold: ' num2str(thres)]);
 disp(['Threshold: ' num2str(thres*255)]);
